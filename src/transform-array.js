61509c33661f5245095e53f8a8cd5043db0afc05
agm234 <79218134+agm234@users.noreply.github.com>
2021-03-27 19:18:35 +0300

    feat: add code

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+ let mass = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 0 && (arr[i] === '--double-prev' || arr[i] === '--discard-prev')) {
+      mass.push(1);
+      mass.splice(0, 1);
+    }
+    else if (i === arr.length - 1 && (arr[i] === '--double-next' || arr[i] === '--discard-next')) {
+      mass.push(1);
+      mass.splice(mass.length - 1, 1);
+    }
+    else if (arr[i] === '--double-prev') {
+      mass.push(arr[i - 1]);
+    }
+    else if (arr[i] === '--double-next') {
+      mass.push(arr[i + 1]);
+    }
+    else if (arr[i - 1] === '--discard-next') {
+      mass.push(1);
+      mass.splice(mass.length - 2, 2);
+      if (arr[i + 1] === '--double-prev' || arr[i + 1] === '--discard-prev') {
+        i = i + 1;
+      }
+    }
+    else if (arr[i] === '--discard-prev') {
+      mass.splice(mass.length - 1, 1);
+    }
+    else {
+      mass.push(arr[i])
+    }
+  }
+  return mass;
 };
